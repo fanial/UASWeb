4b51831825daaa22366b52ca4c070839a6e9085c
Fani Aghistiya Lestari <60648921+fanial@users.noreply.github.com>
2021-06-11 11:45:36 +0700

    database

--- siakad_langitinspirasi.sql
+++ siakad_langitinspirasi.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `siakad_langitinspirasi`
+-- Database: `siakad_uas`
 --
 
 -- --------------------------------------------------------
